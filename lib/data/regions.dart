46fe6c7db80914f862751ea14a7bd7c5f8494769
Kanjiu Akuma <18683538+KanjiuAkuma@users.noreply.github.com>
2020-08-27 21:24:49 +0200

    Added servers to regions

--- lib/data/regions.dart
+++ lib/data/regions.dart
@@ -2,10 +2,10 @@
 /// Created by Kanjiu Akuma on 8/27/2020.
 ///
 
-final Map<String, String> regions = {
-  'EU' : 'Europe',
-  'NA': 'North America',
-  'KR': 'Korea',
-  'JP': 'Japan',
-  'TW': 'Taiwan',
+final Map<String, Map<String, dynamic>> regions = {
+  'EU' : {'name': 'Europe', 'servers': ['Mystel', 'Seren', 'Shakan', 'Shen', 'Yurian']},
+  'NA': {'name': 'North America', 'servers': ['Kaiator', 'Velika']},
+  // 'KR': 'Korea',
+  // 'JP': 'Japan',
+  // 'TW': 'Taiwan',
 };
