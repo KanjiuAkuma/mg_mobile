cab65b1bc2fe69bf3b85a7db486d477c2d7b44fd
Kanjiu Akuma <18683538+KanjiuAkuma@users.noreply.github.com>
2020-08-31 12:51:16 +0200

    Updated boss data (FA HM)

--- util/monsters/update_monsters.py
+++ util/monsters/update_monsters.py
@@ -8,7 +8,7 @@ import json
 
 
 input_path = '../TeraDpsMeterData/monsters/monsters-EU-EN.xml'
-output_path = 'data/monsters.json'
+output_paths = ['data/monsters.json', '../../assets/data/monsters.json']
 
 if __name__ == '__main__':
     # get list of supported ids
@@ -21,23 +21,29 @@ if __name__ == '__main__':
             zone_id = e['AreaId']
             boss_ids = e['BossIds']
             # check for 'special bosses'
+            zone_monsters = {}
             if zone_id == 3034:  # rk9 hm
                 boss_ids = [b_id for b_id in boss_ids if b_id != 4000]
 
             zoneXml = soup.find('zone', id=zone_id)
-            zone_name = zoneXml['name']
-            zone_monsters = {boss_id: zoneXml.find('monster', id=boss_id)['name'] for boss_id in boss_ids}
+            # check for 'special areas'
+            if zone_id == 3203:  # FA hm
+                zone_name = 'Forbidden Arena (Hard)'
+            else:
+                zone_name = zoneXml['name']
 
-            # re add 'special' monsters
+            zone_monsters.update(**{str(boss_id): zoneXml.find('monster', id=boss_id)['name'] for boss_id in boss_ids})
+
             if zone_id == 3034:  # rk9 hm
-                zone_monsters[4000] = zone_monsters[3000] + '+'
+                zone_monsters['4000'] = zone_monsters['3000'] + '+'
 
             monsters[zone_id] = {
                 'version': version,
                 'name': zone_name,
-                'monsters': zone_monsters,
+                'monsters': dict(sorted(zone_monsters.items())),
             }
-        with open(output_path, 'w') as o:
-            json.dump(monsters, o)
+        for p in output_paths:
+            with open(p, 'w') as o:
+                json.dump(monsters, o)
 
 
