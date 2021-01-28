dc865cc30152e92d83e122f6ad3feb268bc926c6
Kami-Kaze <18683538+KanjiuAkuma@users.noreply.github.com>
2021-01-28 20:59:37 +0100

    update monsters

--- util/monsters/update_monsters.py
+++ util/monsters/update_monsters.py
@@ -22,26 +22,22 @@ if __name__ == '__main__':
             boss_ids = e['BossIds']
             # check for 'special bosses'
             zone_monsters = {}
-            if zone_id == 3034:  # rk9 hm
-                boss_ids = [b_id for b_id in boss_ids if b_id != 4000]
 
-            zoneXml = soup.find('zone', id=zone_id)
+            zone_xml = soup.find('zone', id=zone_id)
             # check for 'special areas'
-            if zone_id == 3203:  # FA hm
-                zone_name = 'Forbidden Arena (Hard)'
-            else:
-                zone_name = zoneXml['name']
+            zone_name = zone_xml['name']
 
-            zone_monsters.update(**{str(boss_id): zoneXml.find('monster', id=boss_id)['name'] for boss_id in boss_ids})
-
-            if zone_id == 3034:  # rk9 hm
-                zone_monsters['4000'] = zone_monsters['3000'] + '+'
+            zone_monsters.update(**{str(boss_id): zone_xml.find('monster', id=boss_id)['name'] for boss_id in boss_ids})
 
             monsters[zone_id] = {
                 'version': version,
                 'name': zone_name,
                 'monsters': dict(sorted(zone_monsters.items())),
             }
+
+        print('Monsters:')
+        print(monsters)
+
         for p in output_paths:
             with open(p, 'w') as o:
                 json.dump(monsters, o)
